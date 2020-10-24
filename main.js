935c6bc19637e3e98a6e47c9d6a60787182cfa9d
Mantas Lengvinas <mantaslengvin@gmail.com>
2020-10-24 18:31:53 +0300

    Project init

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let express = require("express")
-let fs = require('fs')
-let http = require("http")
+let express = require('express')
+let bodyParser = require('body-parser')
 
 let app = express()
 let port = 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));  //Public failai (img, css, js)
-app.set('trust proxy', true);
+app.use(bodyParser.json());
 
-app.listen(port, function() {
-    console.log("Serveris paleistas");
-});
+app.get('/', (req, res) => {
+    res.send('Projektas');
+})
 
-app.get('', (req, res) => {
-    res.status(200).send("Labas");
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
 });
