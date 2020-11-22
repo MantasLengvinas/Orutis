9a1cb96317395d88fd5d7e6243fb1ee020cc8898
EimantasVinciunas <e.vinciunas@bankera.lt>
2020-11-22 14:20:23 +0200

    added scrollview

--- FrontEnd/components/screens/RegisterScreen.js
+++ FrontEnd/components/screens/RegisterScreen.js
@@ -2,53 +2,44 @@ import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 import Background from "../background/Background";
-import TextStyles  from "../styles/Text";
-import InputStyles from "../styles/Input"
+import TextStyles from "../styles/Text";
+import InputStyles from "../styles/Input";
 import MyHeader from "../header/MyHeader";
-import { TextInput } from "react-native-gesture-handler";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 import StyledButton from "../buttons/StyledButton";
 import Icon from "../images/Icon";
 
-
 export default function ({ navigation }) {
   return (
-    <Background style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <MyHeader navigation={navigation} goBack={true}/>
-      <Text style={[TextStyles.general, {marginTop: 40}]}>Registracija</Text>
-      <View style={{alignItems: "center", justifyContent: "center", marginTop: 60}}>
-      <TextInput 
-        placeholder="Vartotojo vardas"
-        style={InputStyles.inputField}
-      />
-      <TextInput 
-        placeholder="El. Paštas"
-        style={InputStyles.inputField}
-      />
-      <TextInput
-        secureTextEntry={true} 
-        placeholder="Slaptažodis"
-        style={InputStyles.inputField}
-      />
-      </View>
-      <View style={{alignItems: "center", justifyContent: "center", marginTop: 20}}>
-        <StyledButton style={{marginTop: 20}}>Registruotis</StyledButton>
-        <Text style={styles.privacy}>Registruodamiesi sutinkate su mūsų privatumo politika</Text>
-      </View>
-      <View style={{alignItems: "center", justifyContent: "center", marginTop: 20}}>
-        <Icon/>
-        <Text style={{fontWeight: "bold", color: 'black', fontSize: 20}}>Jau turi paskyrą?</Text>
-      </View>
-    </Background>
+    <ScrollView>
+      <Background style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <MyHeader navigation={navigation} goBack={true} />
+        <Text style={[TextStyles.general, { marginTop: 40 }]}>Registracija</Text>
+        <View style={{ alignItems: "center", justifyContent: "center", marginTop: 60 }}>
+          <TextInput placeholder="Vartotojo vardas" style={InputStyles.inputField} />
+          <TextInput placeholder="El. Paštas" style={InputStyles.inputField} />
+          <TextInput secureTextEntry={true} placeholder="Slaptažodis" style={InputStyles.inputField} />
+        </View>
+        <View style={{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
+          <StyledButton style={{ marginTop: 20 }}>Registruotis</StyledButton>
+          <Text style={styles.privacy}>Registruodamiesi sutinkate su mūsų privatumo politika</Text>
+        </View>
+        <View style={{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
+          <Icon />
+          <Text style={{ fontWeight: "bold", color: "black", fontSize: 20 }}>Jau turi paskyrą?</Text>
+        </View>
+      </Background>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   privacy: {
     fontWeight: "bold",
-    color: 'blue',
+    color: "blue",
     textAlign: "center",
     width: 225,
     marginTop: 20,
-    fontSize: 17
-  }
+    fontSize: 17,
+  },
 });
