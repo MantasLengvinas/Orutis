20fe8c151c05f11e80ca4bfdaac835fab29015fd
EimantasVinciunas <e.vinciunas@bankera.lt>
2020-11-29 21:55:56 +0200

    Update App.js
    commented api

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "./components/screens/HomeScreen";
-import Settings   from "./components/screens/Settings";
+import Settings from "./components/screens/Settings";
 import RegisterScreen from "./components/screens/RegisterScreen";
 import LoginScreen from "./components/screens/LoginScreen";
 import ForgotPassword from "./components/screens/ForgotPassword";
@@ -13,33 +13,41 @@ import Question1 from "./components/screens/questionnaire/Question1";
 
 const Stack = createStackNavigator();
 //weather data
-/*fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+/*fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Vilnius%2Clt&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "17902ac70dmsh670c6ec80cc13c7p19abf5jsna9e0eb240c8d",
 		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
 	}
 })
-.then(response => {
-	console.log(JSON.stringify( response.body));
-})
-.catch(err => {
-	console.error(err);
-});*/
+  .then((response) => {
+    return response.body.getReader();
+  })
+  .then((reader) =>
+    reader.read().then(({ done, value }) => {
+      let data = "";
+      for (let i = 0; i < value.length; i++) {
+        data += String.fromCharCode(value[i]);
+      }
+      data=data.substring(5,data.length-1);
+      console.log(data);
+    })
+  )
+  .catch((err) => {
+    console.error(err);
+  });*/
+
 //==========
 
 export default function App() {
-  
   return (
     <NavigationContainer>
-      
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
-          headerShown: false,     
+          headerShown: false,
         }}
       >
-        
         {/*Screens list from here */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Settings" component={Settings} />
@@ -51,7 +59,6 @@ export default function App() {
         <Stack.Screen name="Question1" component={Question1} />
 
         {/*Screens list end here */}
-        
       </Stack.Navigator>
     </NavigationContainer>
   );
