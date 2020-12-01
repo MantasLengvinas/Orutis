a7770fe7043257ffee7abf742b5d9c7ce4e6ad3f
EimantasVinciunas <e.vinciunas@bankera.lt>
2020-12-01 21:08:39 +0200

    location

--- FrontEnd/components/screens/MainMenu.js
+++ FrontEnd/components/screens/MainMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 import Background from "../background/Background";
@@ -7,7 +7,7 @@ import InputStyles from "../styles/Input";
 import MyHeader from "../header/MyHeader";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import StyledButton from "../buttons/StyledButton";
-import { cos } from "react-native-reanimated";
+import * as Location from "expo-location";
 
 // import DebLiet from "../../assets/raster/WeatherIcons/DebesisLietus.png";
 // import DebLietSnieg from "../../assets/raster/WeatherIcons/DebesisLietusSniegas.png";
@@ -22,47 +22,71 @@ import { cos } from "react-native-reanimated";
 // import SaulDebVejasLiet from "../../assets/raster/WeatherIcons/SauleDebesisVejasLietus.png";
 
 export default function ({ navigation }) {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    //const [orai, updateOrus] = useState({});
-    const [upd, doUpdate] = useState("waiting");
+  const [upd, doUpdate] = useState(null);
 
-    //global.weather = "cring";
-   // let imag = <Image style={styles.icon} source={require("../../assets/raster/WeatherIcons/SauleDebesisVejasLietus.png")} />;
-   //console.log(global.weather);
+  let weather = <></>;
 
-   fetch("http://api.weatherunlocked.com/api/forecast/51.50,-0.12?app_id=b188c162&app_key=62fd3d2f66c74f7b9d1064538c497646")
-   .then(response=> {
-        //console.log(response);
-        response.json().then(data=>{global.weather=data;doUpdate(data.Days[0].date)});
-   })
+  //Get Location================
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+      else{
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      }
+    })();
+  }, []);
+  //=================
 
-   let weather =<></>;
-   if(upd !=="waiting")
-   {
-        console.log(global.weather);
-   }
-   
+  //get weather data=======
+  if (location && !upd) {
+    const lat = Math.round(location.coords.latitude*100)/100;
+    const lon = Math.round(location.coords.longitude*100)/100;
+    console.log(lat +" " +lon);
+    
+    fetch("http://api.weatherunlocked.com/api/forecast/"+lat+","+lon+"?app_id=b188c162&app_key=62fd3d2f66c74f7b9d1064538c497646").then((response) => {
+      //console.log(response);
+      response.json().then((data) => {
+        global.weather = data;
+        doUpdate(data.Days[0].date);
+      });
+    });
+  }
 
-    return (
-            <Background style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <MyHeader navigation={navigation} goBack={false} />
-                <Text style={[TextStyles.general, { marginTop: 40 }]}>Šiandienos orų prognozė</Text>
+  if (errorMsg) {
+  weather=<Text>{errorMsg}</Text>;
+  }
+  if(upd){
+    console.log(global.weather);
+    weather=<Text>weather</Text>;
+  }
+  //===========
 
-                <View style={{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
-                    <Text>{upd}</Text>  
-                </View>
-                
-            </Background>
-    );
+  return (
+    <Background style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <MyHeader navigation={navigation} goBack={false} />
+      <Text style={[TextStyles.general, { marginTop: 40 }]}>Šiandienos orų prognozė</Text>
+
+      <View style={{ alignItems: "center", justifyContent: "center", marginTop: 20 }}>
+        {weather}
+      </View>
+    </Background>
+  );
 }
 
 const styles = StyleSheet.create({
-    privacy: {
-        fontWeight: "bold",
-        color: "blue",
-        textAlign: "center",
-        width: 225,
-        marginTop: 20,
-        fontSize: 17,
-    },
+  privacy: {
+    fontWeight: "bold",
+    color: "blue",
+    textAlign: "center",
+    width: 225,
+    marginTop: 20,
+    fontSize: 17,
+  },
 });
